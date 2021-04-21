4caae39f51fbef4a0b49e5d1aa7f284aa2f881fb
mrlilholt <63080594+mrlilholt@users.noreply.github.com>
2021-04-21 16:39:14 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -36,11 +36,11 @@ f f f f f f f f f f f f f f f f
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
-f f f f f f 1 1 1 1 1 f f f f f 
-f f f f f f 1 1 1 1 1 f f f f f 
-f f f f f f 1 1 1 1 1 f f f f f 
-f f f f f f 1 1 1 1 1 f f f f f 
-f f f f f f 1 1 1 1 1 f f f f f 
+f f f f f f 1 1 1 f f f f f f f 
+f f f f f f 1 1 1 f f f f f f f 
+f f f f f f 1 1 1 f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
